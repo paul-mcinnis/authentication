27baf186ec8ec7cb7527d094521bd5a5338bf45f
Paul McInnis <mcinnis.paul@gmail.com>
2018-03-10 18:48:51 -0500

    Added Model class

---
+++ Auth/Auth.Library/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Auth.Library.Interfaces;
+
+namespace Auth.Library.Models
+{
+    public class User : IUser
+    {
+        public int ModelId { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
