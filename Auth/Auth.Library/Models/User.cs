b41ce930913e8ebd809edddd5c532b95c13b7278
Paul McInnis <mcinnis.paul@gmail.com>
2018-03-22 14:15:03 -0400

    Added date_created and date_modified to db

--- Auth/Auth.Library/Models/User.cs
+++ Auth/Auth.Library/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using Auth.Library.Interfaces;
+﻿using System;
+using Auth.Library.Interfaces;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,10 +12,16 @@ namespace Auth.Library.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ModelId { get; set; }
         
+        [Column("date_created")]
+        public DateTime DateCreated { get; set; }
+        
+        [Column("date_modified")]
+        public DateTime DateModified { get; set; }
+        
         [Column("user_name")]
         [StringLength(50, MinimumLength = 3)]
         public string UserName { get; set; }
-
+        
         [Column("active")]
         public bool? IsActive { get; set; }
     }
