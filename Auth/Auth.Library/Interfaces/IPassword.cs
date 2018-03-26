2f99f6263aa95188b05ff61aa58728950e086e3f
Paul McInnis <mcinnis.paul@gmail.com>
2018-03-26 09:50:36 -0400

    added Password interface

---
+++ Auth/Auth.Library/Interfaces/IPassword.cs
@@ -0,0 +1,15 @@
+﻿namespace Auth.Library.Interfaces
+{
+    public interface IPassword : IModel
+    {
+        /// <summary>
+        /// UserName for User. Is usually Email, but it can be anything
+        /// </summary>
+        string UserName { get; set; }
+        
+        /// <summary>
+        /// saves hashed password
+        /// </summary>
+        string Password { get; set; }
+    }
+}
